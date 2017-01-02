f8abacb4ced51a922556985452063b41ade0111c
Yoko Harada <yokolet@gmail.com>
2017-01-02 13:48:29 -0500

    refactor: Deletes bookstore in light of model change
    Bookstore model was deleted for simplicity. In light of this model
    change, project.py and template have been changed accordingly.
    Also, a template name has been changed to book_catalog.html to
    represent the content well.

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -14,17 +14,20 @@ session = DBSession()
 
 @app.route('/book_catalog/books/JSON')
 def bookCatalogJSON():
-    book_titles = session.query(Book).all()
-    return jsonify(booksotre=[b.serialize for b in book_titles])
+    """Returns all books in JSON format"""
+    books = session.query(Book).all()
+    return jsonify(books=[b.serialize for b in books])
 
 @app.route('/book_catalog/categories/JSON')
 def categoryJSON():
-    book_titles = session.query(Category).all()
-    return jsonify(booksotre=[b.serialize for b in book_titles])
+    """Returns all categories in JSON format"""
+    categories = session.query(Category).all()
+    return jsonify(categories=[b.serialize for b in categories])
 
 
 @app.route('/book_catalog/categories/<int:category_id>/books/JSON')
 def categoryBookJSON(category_id):
+    """Returns all books of a specified category id in JSON format"""
     category= session.query(Category).filter_by(id=category_id).one()
     books = session.query(Book).filter_by(category_id=category_id).all()
     return jsonify(books=[b.serialize for b in books])
@@ -32,41 +35,40 @@ def categoryBookJSON(category_id):
 
 @app.route('/book_catalog/books/<int:book_id>/JSON')
 def bookJSON(book_id):
+    """Returns a book of a specified book id in JSON format"""
     book = session.query(Book).filter_by(id=book_id).one()
     return jsonify(book=book.serialize)
 
 
 @app.route('/')
 @app.route('/book_catalog')
-def showBookTitles():
+def showCategoriesBooks():
+    """Renders all categories and books"""
     categories = session.query(Category).all()
     books = session.query(Book).all()
-    return render_template('bookstores.html',
-                           bookstores=bookstores,
+    return render_template('book_catalog.html',
                            categories=categories,
                            books=books)
 
-@app.route('/bookstores/categories/<int:category_id>')
-def showCategory(category_id):
+@app.route('/book_catalog/categories/<int:category_id>/books')
+def showBooksCategory(category_id):
+    """Renders all books of a specified category id"""
     categories = session.query(Category).all()
     books = session.query(Book).filter_by(category_id=category_id)
-    bookstores = {b.bookstore: 1 for b in books}.keys()
-    return render_template('bookstores.html',
-                           bookstores=bookstores,
+    return render_template('book_catalog.html',
                            categories=categories,
                            books=books)
 
-@app.route('/bookstores/categories/<int:category_id>/books/<int:book_id>')
-def showDescription(category_id, book_id):
+@app.route('/book_catalog/categories/<int:category_id>/books/<int:book_id>')
+def showBookDescription(category_id, book_id):
+    """Renders a description of specified book id"""
     categories = session.query(Category).all()
     if category_id:
         books = session.query(Book).filter_by(category_id=category_id)
     else:
         books = session.query(Book).all()
-    bookstores = {b.bookstore: 1 for b in books}.keys()
     book = filter(lambda b: b.id == book_id, books)[0]
-    return render_template('bookstores.html',
-                           bookstores=bookstores,
+    return render_template('book_description.html',
                            categories=categories,
                            books=books,
                            book=book)
